ab4d5465405cb17d09b73122661f86975d43ff82
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-06-07 12:45:36 -0500

    ya agarra dos enemies

--- Scene1.js
+++ Scene1.js
@@ -1,7 +1,7 @@
 import Survivor from "./Survivor.js";
 import Walker from "./Walker.js";
 import { createAnimations } from "./animations.js";
-
+import WalkerAnimal from "./WalkerAnimal.js";
 export default class Scene1 extends Phaser.Scene {
   constructor() {
     super({ key: "Scene1" }); // ID único de la escena
@@ -10,10 +10,11 @@ export default class Scene1 extends Phaser.Scene {
   preload() {
     this.load.image("streetOne", "assets/scenery/road&lamps_pale.png");
 
-    // this.load.spritesheet("walkerAnimal","assets/characters/zombies/zombieAnimal/Walk.png",
-    //     { frameWidth: 96, 
-    //     frameHeight: 96 
-    // });
+    this.load.spritesheet(
+      "walkerAnimal",
+      "assets/characters/zombies/zombieAnimal/Walk.png",
+      { frameWidth: 96, frameHeight: 96 },
+    );
 
     this.load.spritesheet("survivor", "assets/characters/humans/Walk.png", {
       frameWidth: 128,
@@ -23,24 +24,33 @@ export default class Scene1 extends Phaser.Scene {
       frameWidth: 128,
       frameHeight: 128,
     });
-
   }
 
   create() {
     createAnimations(this);
 
-    this.add.image(0, -100, "streetOne").setOrigin(0, 0).setScale(0.15).setDisplaySize(340, 350);
+    this.add
+      .image(0, -100, "streetOne")
+      .setOrigin(0, 0)
+      .setScale(0.15)
+      .setDisplaySize(340, 350);
 
     this.player = new Survivor(this, 50, 210);
-    this.walker = new Walker(this, 10, 230);
-    //  this.walkerAnimal = new WalkerAnimal(this, 10, 230);
+
+    this.enemies = this.add.group();
+
+    this.enemies.add(new Walker(this, 100, 210));
+    this.enemies.add(new WalkerAnimal(this, 250, 230));
   }
 
   update() {
     if (this.player) this.player.update();
-    if (this.walker) this.walker.update();
 
-    // if (this.walkerAnimal) this.walkerAnimal.update();
+    this.enemies.getChildren().forEach((enemy) => {
+      if (enemy && enemy.update) {
+        enemy.update();
+      }
+    });
 
     // LÓGICA DE CAMBIO: Si el personaje pasa del ancho de la pantalla
     if (this.player.x > this.sys.game.config.width) {
--- animations.js
+++ animations.js
@@ -1,38 +1,47 @@
+// 1. Animaciones específicas del Jugador
+const createSurvivorAnimations = (scene) => {
+  if (!scene.anims.exists("survivor-walks")) {
+    scene.anims.create({
+      key: "survivor-walks",
+      frames: scene.anims.generateFrameNumbers("survivor", { start: 1, end: 6 }),
+      frameRate: 12,
+      repeat: -1,
+    });
+  }
+
+  if (!scene.anims.exists("survivor-idle")) {
+    scene.anims.create({
+      key: "survivor-idle",
+      frames: scene.anims.generateFrameNumbers("survivor", { start: 8, end: 9 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+  }
+};
+
+// 2. Animaciones específicas de los Enemigos
+const createEnemyAnimations = (scene) => {
+  if (!scene.anims.exists("walker-walks")) {
+    scene.anims.create({
+      key: "walker-walks",
+      frames: scene.anims.generateFrameNumbers("walker", { start: 1, end: 9 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+  }
+
+  if (!scene.anims.exists("walker-animal-walks")) {
+    scene.anims.create({
+      key: "walker-animal-walks",
+      frames: scene.anims.generateFrameNumbers("walkerAnimal", { start: 1, end: 9 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+  }
+};
+
+// 3. LA FUNCIÓN PRINCIPAL (La única que exportas)
 export const createAnimations = (scene) => {
-  scene.anims.create({
-    key: "survivor-walks",
-    frames: scene.anims.generateFrameNumbers("survivor", { start: 1, end: 6 }),
-    frameRate: 12,
-    repeat: -1, //repetir infinitamenrte
-  });
-
-  scene.anims.create({
-    key: "survivor-idle",
-    frames: scene.anims.generateFrameNumbers("survivor", { start: 8, end: 9 }),
-    frameRate: 5,
-    repeat: -1,
-  });
-
-//   this.anims.create({
-//     key: "survivor-up",
-//     // frames: [{ key: "mario", frame: 5 }],
-//     frames: scene.anims.generateFrameNumbers("survivor", { start: 1, end: 6 }),
-//     frameRate: 12,
-//     repeat: -1,
-//   });
-
-  scene.anims.create({
-    key: "walker-walks",
-    frames: scene.anims.generateFrameNumbers("walker", { start: 1, end: 9 }),
-    frameRate: 5,
-    repeat: -1,
-  });
-
-  //   scene.anims.create({
-  //   key: "walker-animal-walks",
-  //   frames: scene.anims.generateFrameNumbers("walkerAnimal", { start: 1, end: 9 }),
-  //   frameRate: 5,
-  //   repeat: -1,
-  // });
-
+  createSurvivorAnimations(scene);
+  createEnemyAnimations(scene);
 };
